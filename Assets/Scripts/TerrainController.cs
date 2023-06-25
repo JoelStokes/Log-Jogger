15671baf8ae6dbbc8811ab5b170446e49e71690f
jstokes <joel.stokes.e@gmail.com>
2023-06-24 21:52:57 -0400

    Initial Level Generation

--- Assets/Scripts/TerrainController.cs
+++ Assets/Scripts/TerrainController.cs
@@ -4,40 +4,47 @@ using UnityEngine;
 
 public class TerrainController : MonoBehaviour
 {
+    public GameObject[] EasyPrefabs;
+    public GameObject[] MediumPrefabs;
+    public GameObject[] HardPrefabs;
+
+    public int easyTotal;
+    public int mediumTotal;
+
     private List<GameObject> TerrainChunks = new List<GameObject>();
 
-    private float scrollSpeed = 5;
-    private float destroyX = -20;
-    private float generateX = 20;
-    private bool scrolling = true;
+    private float lastGenX;
+
+    //1. Spawn in prefabs from scene load & place them in order
+    //2. Deactivate Chunk when offscreen for memory
+    //3. Rework collectibles to be regenerated for level chunk pooling
+            //3a. Because of set amount of Easy & Medium Prefabs, only Hard have to be pooled?
 
     void Start()
     {
-        TerrainChunks.Add(transform.GetChild(0).gameObject);
+        lastGenX = transform.position.x;
+        
+        GenerateChunks(EasyPrefabs, easyTotal);
+        //GenerateChunks(MediumPrefabs, mediumTotal);
+        //GenerateChunks(HardPrefabs, HardPrefabs.Length);
     }
 
-    void Update()
-    {
-        if (scrolling){
-            float speed = scrollSpeed * Time.deltaTime;
+    private void GenerateChunks(GameObject[] LevelPrefabs, int total){
+        List<int> usedPrefabs = new List<int>();
+        int random;
 
-            /*for (int i=0; i<TerrainChunks.Count; i++){
-                TerrainChunks[i].transform.position = new Vector3(TerrainChunks[i].transform.position.x - speed, 
-                    TerrainChunks[i].transform.position.y, 
-                    TerrainChunks[i].transform.position.z);
+        for (int i=0; i<total; i++){
+            if (usedPrefabs.Count != LevelPrefabs.Length){  //Prevent infinite loop
+                do {
+                    random = Random.Range(0, LevelPrefabs.Length);
+                } while (usedPrefabs.Contains(random));
 
-                if (transform.position.x < destroyX){
-                    TerrainChunks.RemoveAt(i);
-                }
-            }*/
+                GameObject newObj = GameObject.Instantiate(LevelPrefabs[random], new Vector3(lastGenX, transform.position.y, 0.5f), Quaternion.identity);          
+                
+                Debug.Log(newObj.name + "/Connector");
+                lastGenX = transform.Find(newObj.name + "/Connector").transform.position.x;
+                usedPrefabs.Add(random);
+            }
         }
-    }
-
-    public void PauseScroll(){  //Player Slam, against wall, or death
-        scrolling = false;
-    }
-
-    public void ResumeScroll(){
-        scrolling = true;
     }
 }
