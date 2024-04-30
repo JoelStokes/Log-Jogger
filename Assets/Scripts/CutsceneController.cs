bca687631097ec1a1a7dfa1f6eeb4fb31dcdc3f4
Joel Stokes <joel.stokes.e@gmail.com>
2024-04-30 01:06:34 -0400

    Molehole scene in Intro Cutscene / Falling Rocks in Cave

--- Assets/Scripts/CutsceneController.cs
+++ Assets/Scripts/CutsceneController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutsceneController : MonoBehaviour
 {
     public void SkipCutscene(){
-
+        SceneManager.LoadScene("Title");
     }
 }
